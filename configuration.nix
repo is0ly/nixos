cf9831f371fb1df6ca62afb357a580f7e1b36d67
Ilya Osipov <iljaosipov@gmail.com>
2024-02-29 01:08:31 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,106 +1,148 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "oivoss"; # Define your hostname.
+  networking.hostName = "oivoss"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  networking.networkmanager.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Moscow";
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
-  };
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
 
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-  };
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
 
-  users.users.ilia = {
-    isNormalUser = true;
-    description = "ilia";
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
-    packages = with pkgs; [];
-  };
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ilia = {
+    isNormalUser = true;
+    description = "ilia";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    packages = with pkgs; [];
+  };
 
-  nixpkgs.config.allowUnfree = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-  wget
-  btop
-  lsd
-  neovim
-  git
-  neofetch
-  kitty
-  google-chrome
-  rofi-wayland-unwrapped
-  blueman
-  pavucontrol
-  zsh
-  telegram-desktop
-  starship
-  swww
-  dunst
-  spotify
-  cbonsai
-   ];
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  wget
+  btop
+  lsd
+  neovim
+  git
+  neofetch
+  kitty
+  google-chrome
+  rofi-wayland-unwrapped
+  blueman
+  pavucontrol
+  zsh
+  telegram-desktop
+  starship
+  swww
+  dunst
+  spotify
+  cbonsai
+  ];
 
-  programs.hyprland = {
+  programs.hyprland = {
 	enable = true;
 };
 
 
-  services.openssh.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-   hardware.bluetooth.enable = true;
-   hardware.bluetooth.powerOnBoot = true;
-   services.blueman.enable = true;
+  # List services that you want to enable:
 
-   security.rtkit.enable = true;
-   services.pipewire = {
-   	enable = true;
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  
+ # services.bluetooth.enable = true;
+   hardware.bluetooth.enable = true;
+   hardware.bluetooth.powerOnBoot = true;
+   services.blueman.enable = true;
+   
+   # PipeWire
+   # rtkit is optional but recommended
+   security.rtkit.enable = true;
+   services.pipewire = {
+   	enable = true;
 	alsa.enable = true;
 	alsa.support32Bit = true;
 	pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
 };
 
-  fonts.packages = with pkgs; [
-  noto-fonts
-  noto-fonts-cjk
-  noto-fonts-emoji
-  noto-fonts-color-emoji
-  liberation_ttf
-  fira-code
-  fira-code-symbols
-  mplus-outline-fonts.githubRelease
-  dina-font
-  proggyfonts
-  (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono"]; })
+  fonts.packages = with pkgs; [
+  noto-fonts
+  noto-fonts-cjk
+  noto-fonts-emoji
+  noto-fonts-color-emoji
+  liberation_ttf
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts.githubRelease
+  dina-font
+  proggyfonts
+  (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" "Iosevka"]; })
 ];
 
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
 
-  system.stateVersion = "24.05"; # Did you read the comment?
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
